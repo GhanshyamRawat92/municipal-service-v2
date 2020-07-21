cb512149c47da8cf53057ed413abedfed8db05ed
GhanshyamRawat92 <bittu.aec@gmail.com>
2020-07-22 01:43:29 +0530

    change response entity

--- dcr-service-consumer/src/main/java/org/egov/drc/controller/DcrController.java
+++ dcr-service-consumer/src/main/java/org/egov/drc/controller/DcrController.java
@@ -4,14 +4,17 @@ import org.egov.dcr.web.models.DcrRequest;
 import org.egov.dcr.web.models.DcrResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/dcr")
 public class DcrController {
@@ -28,7 +31,9 @@ public class DcrController {
 	@PostMapping("/_create")
 	public ResponseEntity<DcrResponse> consumeDcr(@RequestBody DcrRequest dcrRequest){
 		System.out.println("consumeDcr:"+dcrRequest);
-		ResponseEntity<DcrResponse> dcrResponse = restTemplate.postForEntity(idGenHost.concat(idGenPath), dcrRequest, DcrResponse.class);
-		return dcrResponse;
+		ResponseEntity<DcrResponse> dcrResponseEntity = restTemplate.postForEntity(idGenHost.concat(idGenPath), dcrRequest, DcrResponse.class);
+		DcrResponse dcrResponse = dcrResponseEntity.getBody();
+		log.info("dcrResponse:"+ dcrResponse);
+		return new ResponseEntity<>(dcrResponse, HttpStatus.CREATED);
 	}
 }
